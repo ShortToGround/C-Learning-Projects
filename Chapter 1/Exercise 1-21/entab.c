1d0670e49da47f4a139a4bed753bc00729949b88
James <68284618+ShortToGround@users.noreply.github.com>
2020-08-06 23:00:08 -0500

    Changed wording for clarity

--- Chapter 1/Exercise 1-21/entab.c
+++ Chapter 1/Exercise 1-21/entab.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-#define TABBLOCK		8			// Tab length in spaces
+#define TABSTOP		8			// Tab length in spaces
 
 
 int main(void){
 	
-	int c;						// Holds the current character
-	int columnpos = 1; 				// Cycles though the current position of the char relative to the tab block - Starts on col 1
+	int c;							// Holds the current character
+	int columnpos = 1; 				// Cycles though the current position of the char relative to the tab stop - Starts on col 1
 	int spaces = 0;					// Keeps track of the number of spaces entered in a row
 	int tabsOut;					// Number of tabs that will be output
 	int spacesOut;					// Number of spaces that will be output
 	int spaceStart;					// The column position where a space is first entered
-	int nextBlock;					// The next tab block column position relative to the current character
+	int nextBlock;					// The next tab stop column position relative to the current character
 	
 	while ((c = getchar()) != EOF){
 		
@@ -22,31 +22,31 @@ int main(void){
 			else{
 				spaceStart = columnpos;
 				++spaces;
-				// Calculates the next tab block location, vital for outputing the correct data
-				nextBlock = columnpos + (TABBLOCK - (columnpos % TABBLOCK));
+				// Calculates the next tab stop location, vital for outputing the correct data
+				nextBlock = columnpos + (TABSTOP - (columnpos % TABSTOP));
 			}
 		}
 		else{
 			if (spaces > 0){
-				// If the spaces aren't enough to get to the next tab block, just print them all
+				// If the spaces aren't enough to get to the next tab stop, just print them all
 				if (((spaceStart - 1) + spaces) < nextBlock){
 					for (; spaces > 0; --spaces){
 						putchar(' ');	
 					}
 					putchar(c);
 				}
-				// If the spaces at least reach the next tab block
+				// If the spaces at least reach the next tab stop
 				else{
 					// We're going to insert a tab to replace the spaces from the
-					// 	spaceStart column to the next tab block
+					// 	spaceStart column to the next tab stop
 					
 					// Here we adjust the space counter for the spaces that are to be removed by the next tab
 					spaces = spaces - (nextBlock - (spaceStart - 1));
 					// Put the first tab, now the rest of the calculation will be a little easier
-					// Since we are starting on a tab block
+					// Since we are starting on a tab stop
 					putchar('\t');
-					spacesOut = (spaces % TABBLOCK);
-					tabsOut = ((spaces - spacesOut) / TABBLOCK);
+					spacesOut = (spaces % TABSTOP);
+					tabsOut = ((spaces - spacesOut) / TABSTOP);
 					// Output the calculated tabs
 					for (; tabsOut > 0; --tabsOut){
 						putchar('\t');	
